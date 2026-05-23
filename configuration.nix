a3bb1f7ecbb086614d9cabc8c0a40801a70cb737
YanRui06 <yanrui060127@outlook.com>
2026-05-24 02:41:55 +0800

    更新配置，添加系统级静态代理设置以支持网络请求

--- configuration.nix
+++ configuration.nix
@@ -124,6 +124,14 @@
     5900
   ];
 
+  # 系统级静态代理
+  networking.proxy = {
+    default = "http://127.0.0.1:7897";
+    httpProxy = "http://127.0.0.1:7897";
+    httpsProxy = "http://127.0.0.1:7897";
+    noProxy = "localhost,127.0.0.1,::1,*.local";
+  };
+
   # 禁用所有形式的休眠 (配合某些硬件休眠唤醒可能出问题)
   systemd.sleep.extraConfig = ''
     AllowSuspend=yes         # 允许睡眠
