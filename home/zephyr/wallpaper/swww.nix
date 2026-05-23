e6f192712306ecb9ba4978777919b7486f4ece71
YanRui06 <yanrui060127@outlook.com>
2026-05-23 23:51:08 +0800

    更新配置，添加壁纸切换脚本并优化环境变量设置

--- home/zephyr/wallpaper/swww.nix
+++ home/zephyr/wallpaper/swww.nix
@@ -7,4 +7,14 @@
     source = ./image;
     recursive = true;
   };
+
+  home.file.".config/wallpaper/swww-rofi.sh" = {
+    text = ''
+      #!/bin/sh
+      # 简单的壁纸切换逻辑示例，具体根据你的脚本内容调整
+      # 这里只是确保文件存在且可执行
+      swww img $(find ~/.config/wallpaper -type f | shuf -n 1)
+    '';
+    executable = true;
+  };
 }
