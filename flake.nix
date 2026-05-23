d9cc2fb7b848e33dfcad539838c1f450b3c663cc
YanRui06 <yanrui060127@outlook.com>
2026-05-23 21:53:11 +0800

    添加 home-manager 备份文件扩展名配置，移除默认备份文件扩展名设置

--- flake.nix
+++ flake.nix
@@ -65,6 +65,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "bak";
             home-manager.extraSpecialArgs = {inherit inputs pkgs-unstable;};
             home-manager.users.zephyr = {
               imports = [
--- home/zephyr/default.nix
+++ home/zephyr/default.nix
@@ -91,6 +91,6 @@
   };
 
   home.stateVersion = "25.11";
-  home.backupFileExtension = "bak";
+  # home.backupFileExtension = "bak";
   # home.enableNixpkgsReleaseCheck = false;
 }
