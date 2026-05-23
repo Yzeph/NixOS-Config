80b8551ff885fe40166f207ad6658aac0f91ce7d
YanRui06 <yanrui060127@outlook.com>
2026-05-24 01:01:34 +0800

    更新配置，移除 zsh 别名中的重复代理手动开关命令，并将 NixOS 维护命令部分移至适当位置

--- home/zephyr/shell/zsh.nix
+++ home/zephyr/shell/zsh.nix
@@ -42,20 +42,12 @@
       ff = "fastfetch";
       lg = "lazygit";
       
-      # 代理手动开关
-      proxy = "export http_proxy=http://127.0.0.1:7897 https_proxy=http://127.0.0.1:7897 all_proxy=socks5://127.0.0.1:7897";
-      unproxy = "unset http_proxy https_proxy all_proxy";
-
-      # NixOS 维护命令
-      nrs = "sudo nixos-rebuild switch --flake .#zephyr";
-      nlg = "sudo nix-env -p /nix/var/nix/profiles/system --list-generations";
-      ncg = "sudo nix-collect-garbage -d";
-      nopt = "sudo nix-store --optimise"; # 优化存储（去重）
-
       # 代理控制
       proxy = "export http_proxy=http://127.0.0.1:7897 https_proxy=http://127.0.0.1:7897 all_proxy=socks5://127.0.0.1:7897 no_proxy='localhost,127.0.0.1,::1,.local'";
       unproxy = "unset http_proxy https_proxy all_proxy";
       proxy_status = "env | grep -E 'proxy|PROXY'";
+
+      # NixOS 维护命令
     };
 
     # Oh My Zsh 框架
