15128ebde085edd969ecefc268d73346a1896b07
Zephyr <yanrui060127@outlook.com>
2026-07-16 18:06:10 +0800

    修复shellAliases路径问题

--- home-manager/zephyr.nix
+++ home-manager/zephyr.nix
@@ -49,8 +49,8 @@
       ll  = "ls -l";
       la  = "ls -la";
       lt  = "ls -lart";
-      rebuild = "cd /home/zephyr/nix-config && sudo nixos-rebuild switch --flake .#nixos";
-      update  = "cd /home/zephyr/nix-config && sudo nix flake update";
+      rebuild = "cd /home/zephyr/nixos-config && sudo nixos-rebuild switch --flake .#nixos";
+      update  = "cd /home/zephyr/nixos-config && sudo nix flake update";
       gc      = "sudo nix-collect-garbage --delete-older-than 7d";
     };
     oh-my-zsh = {
